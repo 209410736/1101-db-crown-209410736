532a37b751eddb10cc56ec3cf6bbaff2410f35a1
209410736 <209410736@gms.tku.edu.tw>
2021-10-14 08:42:02 +0800

    Initial commit

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,14 @@
 const { Pool} = require('pg');
 
+const isProduction = process.env.NODE_ENV ==="production"
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'crown_36',
-  password: '0000',
-  port: 5432,
+  //user: 'postgres',
+  //host: 'localhost',
+ // database: 'crown_36',
+  //password: '0000',
+ // port: 5432,
+ connectionString: isProduction ? process.env.DATABASE_URL : `postgresql://postgres:0000@localhost:5432/crown_36`
 })
 
 pool.query('SELECT * from category_36', (err, res) => {
